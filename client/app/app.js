2d4287ce1f8031d86458240e8fa9f21d4f068b23
Ricky Walker <rickwalk45@gmail.com>
2015-09-22 20:55:02 -0700

    reconfigured package.json files for the express server

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,35 @@
 angular.module('orangutangular', ['ngRoute'])
+.config(function($routeProvider){
+  $routeProvider
+    // .when('/', {
+    //   templateUrl: '',
+    //   // controller: 'OpeningController'
+    // })
+    .when('/intro', {
+      templateUrl: 'app/views/intro.html',
+      // controller: 'IntroController'
+    })
+    .when('/castle', {
+      templateUrl: 'app/views/castle.html',
+      // controller: 'IntroController'
+    })
+    .when('/lair', {
+      templateUrl: 'app/views/lair.html',
+      // controller: 'IntroController'
+    })
+    .when('/victory', {
+      templateUrl: 'app/views/victory.html',
+      // controller: 'IntroController'
+    })
+    .when('/ending', {
+      templateUrl: 'app/views/ending.html',
+      // controller: 'IntroController'
+    })
+    .when('/intro', {
+      templateUrl: 'app/views/intro.html',
+      // controller: 'IntroController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+})
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var server = express();
 
-server.use(express.static('client'));
+server.use(express.static(__dirname + '/client'));
 
-server.listen(8000);
+server.listen(8000, function(){
+  console.log('listening on 8000...')
+});
